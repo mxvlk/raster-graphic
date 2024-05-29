80fd1a5fedccab507c733b2c4e554ab29d4e42c8
Maximilian Volk <66070350+mxvlk@users.noreply.github.com>
2024-05-29 22:40:33 +0200

    Create draw.h

---
+++ draw.h
@@ -0,0 +1,15 @@
+#define X_PIXEL 600
+#define Y_PIXEL X_PIXEL
+#define ARRAY_DEPTH 4
+#define FILE_NAME "graphic.ppm"
+#define COLOR_R 0
+#define COLOR_G 255
+#define COLOR_B 0
+#define PICTURE_PADDING 10
+
+void createPpm();
+void setPixel();
+void setPixelWithDirection();
+void drawLine();
+void drawSquare();
+void drawFigure();
